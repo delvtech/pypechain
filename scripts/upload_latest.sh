45c7bfbbd5a453f6c688d29d28fa68ffacae1382
Matthew Brown <mattfdbrown@gmail.com>
2023-11-08 13:46:58 -0700

    add script to upload

--- scripts/upload_latest.sh
+++ scripts/upload_latest.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
-# Change directory to the dist folder
-cd dist
+# Source the .env file to export the TWINE_USERNAME and TWINE_PASSWORD
+if [ -f ".env" ]; then
+    export $(cat .env | xargs)
+else
+    echo ".env file not found"
+    exit 1
+fi
 
-# Find the latest package version, assuming the naming convention is like 'pypechain-VERSION'
-# Sorts by version number and gets the last one
-latest_package=$(ls pypechain-* | sort -V | tail -n 1)
+# Navigate to the dist directory
+cd dist || exit 1
 
-# Extract only the version part from the filename
-latest_version=$(echo "$latest_package" | sed -E 's/pypechain-(.*)\.tar\.gz/\1/')
+# Find the latest version of the package, assuming the naming convention is 'pypechain-VERSION'
+# This will extract the version numbers, sort them, and get the highest version
+latest_version=$(ls pypechain-*.tar.gz | sort -V | tail -n 1 | sed -E 's/pypechain-(.*)\.tar\.gz/\1/')
 
-# Use twine to upload the latest version of the package
-# The glob pattern will match all files for the given version
+# Check if latest_version is not empty
+if [[ -z "$latest_version" ]]; then
+    echo "No distribution files found for upload."
+    exit 1
+fi
+
+# Use twine to upload all packages for the latest version
+# The * after the version number is important to match all files for the version
 twine upload "pypechain-${latest_version}"*
 
-# Change back to the original directory if needed
+# Navigate back to the original directory
 cd -
