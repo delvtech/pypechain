f21ea9dc05c92fd1b9f8affaabb09e3308c23c4f
Matthew Brown <mattfdbrown@gmail.com>
2023-11-15 20:07:32 -0700

    add test fixtures

--- conftest.py
+++ conftest.py
@@ -6,6 +6,10 @@ import time
 from typing import Iterator
 
 import pytest
+from eth_typing import URI
+from web3 import Web3
+from web3.middleware import geth_poa
+from web3.types import RPCEndpoint
 
 
 @pytest.fixture(scope="function")
@@ -26,7 +30,7 @@ def local_chain() -> Iterator[str]:
     # Using context manager here seems to make CI hang, so explicitly killing process at the end of yield
     # pylint: disable=consider-using-with
     anvil_process = subprocess.Popen(
-        ["anvil", "--host", "127.0.0.1", "--port", str(anvil_port), "--code-size-limit", "9999999999"]
+        ["anvil", "--silent", "--host", "127.0.0.1", "--port", str(anvil_port), "--code-size-limit", "9999999999"]
     )
 
     local_chain_ = "http://" + host + ":" + str(anvil_port)
@@ -38,3 +42,52 @@ def local_chain() -> Iterator[str]:
 
     # Kill anvil process at end
     anvil_process.kill()
+
+
+@pytest.fixture(scope="function")
+def w3(local_chain) -> Web3:  # pylint: disable=redefined-outer-name
+    """gets a Web3 instance connected to the local chain.
+
+    Parameters
+    ----------
+    local_chain : str
+        A local anvil chain.
+
+    Returns
+    -------
+    Web3
+        A web3.py instance.
+    """
+
+    return initialize_web3_with_http_provider(local_chain)
+
+
+def initialize_web3_with_http_provider(
+    ethereum_node: URI | str, request_kwargs: dict | None = None, reset_provider: bool = False
+) -> Web3:
+    """Initialize a Web3 instance using an HTTP provider and inject a geth Proof of Authority (poa) middleware.
+
+    Arguments
+    ---------
+    ethereum_node: URI | str
+        Address of the http provider
+    request_kwargs: dict
+        The HTTPProvider uses the python requests library for making requests.
+        If you would like to modify how requests are made,
+        you can use the request_kwargs to do so.
+
+    Notes
+    -----
+    The geth_poa_middleware is required to connect to geth --dev or the Goerli public network.
+    It may also be needed for other EVM compatible blockchains like Polygon or BNB Chain (Binance Smart Chain).
+    See more `here <https://web3py.readthedocs.io/en/stable/middleware.html#proof-of-authority>`_.
+    """
+    if request_kwargs is None:
+        request_kwargs = {}
+    provider = Web3.HTTPProvider(ethereum_node, request_kwargs)
+    web3 = Web3(provider)
+    web3.middleware_onion.inject(geth_poa.geth_poa_middleware, layer=0)
+    if reset_provider:
+        # TODO: Check that the user is running on anvil, raise error if not
+        _ = web3.provider.make_request(method=RPCEndpoint("anvil_reset"), params=[])
+    return web3
