0315046f1d34f9c1e6c54f536a4e1124788c63e9
Matthew Brown <mattfdbrown@gmail.com>
2023-11-30 16:22:54 -0800

    convert dataclasses to tuples

--- pypechain/utilities/templates.py
+++ pypechain/utilities/templates.py
@@ -10,4 +10,22 @@ def get_jinja_env() -> Environment:
     # Construct the path to your templates directory.
     templates_dir = os.path.join(script_dir, "../templates")
     env = Environment(loader=FileSystemLoader(templates_dir))
+    env.filters["wrap_with_dataclass_to_tuple"] = wrap_with_dataclass_to_tuple
+
     return env
+
+
+def wrap_with_dataclass_to_tuple(items: list[str]) -> list[str]:
+    """Wraps a string with with the tuple_to_dataclass function.
+
+    Parameters
+    ----------
+    items: list[str]
+        a list of variables to wrap with the function `dataclass_to_tuple`
+
+    Returns
+    -------
+    list[str]
+        A list of wrapped strings.
+    """
+    return [f"dataclass_to_tuple({item})" for item in items]
