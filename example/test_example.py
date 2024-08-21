044bf755b01957df22179042e5339fc6276246f9
Dylan <dpaiton@gmail.com>
2024-08-21 10:53:32 -0700

    more lint errors

--- example/test_example.py
+++ example/test_example.py
@@ -13,6 +13,8 @@ from example.types import ExampleContract
 from example.types.ExampleTypes import InnerStruct, NestedStruct, SimpleStruct
 from pypechain.utilities.abi import load_abi_infos_from_file
 
+# it exists, we promise
+# pylint: disable=no-name-in-module
 from .types import ExampleFlipFlopContractFunction
 
 # using pytest fixtures necessitates this.
