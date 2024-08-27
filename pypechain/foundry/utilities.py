efd7d22cf14c4110f05298a837ef8471c2c98b78
Sheng Lundquist <sheng@delv.tech>
2024-08-27 11:15:44 -0700

    Moving abi check to `is_foundry_json`

--- pypechain/foundry/utilities.py
+++ pypechain/foundry/utilities.py
@@ -8,7 +8,10 @@ from pypechain.foundry.types import FoundryJson
 def is_foundry_json(val: object) -> TypeGuard[FoundryJson]:
     """Determines whether a json object is a FoundryJson."""
     required_keys = {"abi", "bytecode", "deployedBytecode", "methodIdentifiers", "rawMetadata", "metadata", "id"}
-    return isinstance(val, dict) and required_keys.issubset(val.keys())
+    has_keys = isinstance(val, dict) and required_keys.issubset(val.keys())
+    # Only support files with abis
+    has_abi = len(val["abi"]) > 0
+    return has_keys and has_abi
 
 
 def get_bytecode_from_foundry_json(json_abi: FoundryJson) -> str:
