4097aa02c0d5c458f055f3793c0551501e7184a6
Sheng Lundquist <sheng@delv.tech>
2024-08-29 11:34:45 -0700

    Adding solc types to bytecode functions

--- pypechain/utilities/bytecode.py
+++ pypechain/utilities/bytecode.py
@@ -10,12 +10,13 @@ from pypechain.foundry.utilities import (
 )
 from pypechain.hardhat.types import HardhatJson
 from pypechain.hardhat.utilities import get_bytecode_from_hardhat_json, is_hardhat_json
+from pypechain.solc.types import SolcJson
 from pypechain.solc.utilities import get_bytecode_from_solc_json, is_solc_json
 
 from .types import LinkReferences
 
 
-def get_bytecode_from_json(json_abi: HardhatJson | FoundryJson) -> str:
+def get_bytecode_from_json(json_abi: HardhatJson | FoundryJson | SolcJson) -> str:
     """Gets the bytecode from any supported json format."""
     if is_foundry_json(json_abi):
         return get_bytecode_from_foundry_json(json_abi)
@@ -27,7 +28,7 @@ def get_bytecode_from_json(json_abi: HardhatJson | FoundryJson) -> str:
     raise ValueError("Unable to retrieve bytecode, JSON in unknown format.")
 
 
-def get_bytecode_link_references_from_json(json_abi: HardhatJson | FoundryJson) -> list[LinkReferences]:
+def get_bytecode_link_references_from_json(json_abi: HardhatJson | FoundryJson | SolcJson) -> list[LinkReferences]:
     """Gets the bytecode from any supported json format."""
     if is_foundry_json(json_abi):
         return get_bytecode_link_references_from_foundry_json(json_abi)
